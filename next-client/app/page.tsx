f209d6ad744a6f22251ab55c3c03828ec72d7f38
Sayuru Jithsara <sayuru@badbyte.cc>
2024-07-07 13:44:32 +0530

    Reimported deps

--- next-client/app/page.tsx
+++ next-client/app/page.tsx
@@ -1,9 +1,13 @@
+import About from "@/components/about";
+import Footer from "@/components/footer";
+import { Header } from "@/components/Header";
+import { Hero } from "@/components/Hero";
 import Timeline from "@/components/server-side/Timeline";
 import React from "react";
 
 export default function Home() {
   return (
-    <main className="h-[300vh]">
+    <main>
       <Header />
       <Hero />
       <Timeline />
